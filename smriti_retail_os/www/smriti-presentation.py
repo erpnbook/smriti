d9bb536162573b51576b3dec9630cb620770f5e8
ganitsutram-ui <ganitsutram@gmail.com>
2026-06-27 20:19:09 +0530

    chore: align version headers in smriti-presentation to v1.8.4

--- smriti_retail_os/www/smriti-presentation.py
+++ smriti_retail_os/www/smriti-presentation.py
@@ -4,7 +4,7 @@
 # @description: Page controller for SMRITI Retail OS Business Owner Presentation.
 # @author: Jawahar R Mallah <jawahar.mallah@gmail.com>
 # @date: 2026-06-22
-# @version: 1.0.0
+# @version: 1.8.4
 # @license: MIT
 # * Copyright (c) 2026 AITDL NETWORK & ERPNbook.com. All rights reserved.
 #
