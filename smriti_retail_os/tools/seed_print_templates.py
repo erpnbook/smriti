655499b3445566d5a9e92df829e1c2d7f4705b9f
SMRITI Retail OS <admin@erpnbook.com>
2026-07-24 17:52:05 +0530

    feat(barcode): add default TattlyThreads PRN barcode label template

--- smriti_retail_os/tools/seed_print_templates.py
+++ smriti_retail_os/tools/seed_print_templates.py
@@ -8,6 +8,109 @@ from smriti_retail_os import smriti
 
 DEFAULT_TEMPLATES = [
     {
+        "name": "TattlyThreads",
+        "template_title": "TattlyThreads",
+        "label_size": "106x55",
+        "printer_language": "ZPL",
+        "printer_family": "ZPL",
+        "custom_is_default": 1,
+        "custom_active": 1,
+        "raw_template": """<xpml><page quantity='0' pitch='50.7 mm'></xpml>^XA
+^SZ2^JMA
+^MCY^PMN
+^PW804
+^JZY
+^LH0,0^LRN
+^XZ
+<xpml></page></xpml><xpml><page quantity='1' pitch='50.7 mm'></xpml>^XA
+^FO346,305
+^BY2^BCN,66,N,N^FD{barcode}^FS
+^FT390,385
+^CI0
+^AAN,27,15^FD{barcode}^FS
+^FT772,357
+^A0B,34,46^FDTATTLY THREADS^FS
+^FT355,271
+^ADN,18,10^FD81,Umerkhadi,Mumbai,400003^FS
+^FT355,289
+^ADN,18,10^FDcare@tattlythreads.com^FS
+^FO627,62
+^GB70,67,67^FS
+^FT627,116
+^A0N,65,72^FR^FD{size}^FS
+^FT405,111
+^A0N,37,49^FD{color}^FS
+^FO416,15
+^GB284,47,47^FS
+^FT416,54
+^A0N,45,44^FR^FD{style}     ^FS
+^FO332,13
+^GB367,117,3^FS
+^FO334,57
+^GB337,0,3^FS
+^FT490,199
+^A0N,17,23^FD |(Incl of all taxes)^FS
+^FT488,175
+
+
+^A0N,42,56^FD{mrp}/-^FS
+^FT408,170
+^A0N,28,38^FDMRP:^FS
+^FT355,199
+^A0N,17,23^FDMFG.Dt.:{pkd_date}^FS
+^FT355,215
+^ABN,11,7^FDNET CONTENTS:1 Pair Footwear^FS
+^FT340,41
+^A0N,17,23^FDArt.No.^FS
+^FT340,103
+^A0N,17,23^FDColor:^FS
+^FO34,112
+^BY1^BCN,30,N,N^FD{barcode}^FS
+^FT26,165
+^A0N,25,34^FD{barcode}^FS
+^FO37,47
+^GB70,67,67^FS
+^FT37,101
+^A0N,65,72^FR^FD{size}^FS
+^FT116,63
+^A0N,28,38^FD{color}^FS
+^FT37,34
+^A0N,28,27^FD{style}^FS
+^FT17,146
+^ABB,11,7^FDTATTLY THREADS^FS
+^FT116,84
+^A0N,20,27^FDMRP:{mrp}/-^FS
+^FT116,101
+^A0N,17,23^FD(Incl of all taxes)^FS
+^FO33,338
+^BY1^BCN,30,N,N^FD{barcode}^FS
+^FT26,394
+^A0N,25,34^FD{barcode}^FS
+^FO33,274
+^GB70,67,67^FS
+^FT33,328
+^A0N,65,72^FR^FD{size}^FS
+^FT116,289
+^A0N,28,38^FD{color}^FS
+^FT33,260
+^A0N,28,27^FD{style}^FS
+^FT16,372
+^ABB,11,7^FDTATTLY THREADS^FS
+^FT116,310
+^A0N,20,27^FDMRP:{mrp}/-^FS
+^FT116,327
+^A0N,17,23^FD(Incl of all taxes)^FS
+^FO731,0
+^GB0,405,3^FS
+^FO324,236
+^GB407,0,3^FS
+^FT355,261
+^A0N,20,27^FDMKTD.By:Tattly Threads^FS
+^PQ1,0,1,Y
+^XZ
+<xpml></page></xpml><xpml><end/></xpml>"""
+    },
+    {
         "name": "Retail Footwear Dual Tag (50x25)",
         "template_title": "Retail Footwear Dual Tag (50x25)",
         "label_size": "50x25",
